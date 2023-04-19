96b239f13e1122ecef8c3725632cd6bd5b3d8378
Vivek Sawant <109336234+vivekedulab@users.noreply.github.com>
2023-04-19 12:27:57 +0530

    for testing in app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload the app given app
+          Edit <code>src/App.js</code> and save to reload the app given app for testing
         </p>
         <a
           className="App-link"
