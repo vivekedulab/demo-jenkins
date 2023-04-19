a6aa0ff83776eef28462a67c015aa80d440dd9da
Vivek Sawant <109336234+vivekedulab@users.noreply.github.com>
2023-04-19 13:06:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload the app with star
+          Edit <code>src/App.js</code> and save to reload the app with star any 
         </p>
         <a
           className="App-link"
